4cb06b88347e1f3b556462935710c0b02cdca0a3
apdallah <apdallah.yosri@gmail.com>
2020-06-02 02:00:34 +0200

    Create breakingRecords.kt

---
+++ breakingRecords.kt
@@ -0,0 +1,51 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the breakingRecords function below.
+fun breakingRecords(scores: Array<Int>): Array<Int> {
+   val result= arrayOf(0,0)
+    var currentMin=scores[0]
+    var currentMax=scores[0]
+    scores.forEach { 
+            if(it>currentMax){
+                result[0]+=1
+                currentMax=it
+            }
+            
+            if(it<currentMin){
+                result[1]++
+                currentMin=it
+            }
+     }
+     return result
+
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val scores = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = breakingRecords(scores)
+
+    println(result.joinToString(" "))
+}
